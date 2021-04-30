c3c10c282f2c010ecfbe7b0bc04cf79e5eb45df5
Quentin-BLANC <quentinblanc587@gmail.com>
2021-04-30 22:04:02 +0200

    05 - Symfony : Le routing avancé

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,16 +5,27 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @Route("/programs", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
      * @return Response
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+            'website' => 'Wild Séries']);
+    }
+
+    /**
+     * @Route("/{id<\d+>}/", methods={"GET"}, name="show")
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
     }
 }
