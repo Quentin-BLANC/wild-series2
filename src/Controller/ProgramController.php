98641a9296190d83a6da1d11f8b5760d71b9c348
Quentin-BLANC <quentinblanc587@gmail.com>
2021-05-25 22:09:05 +0200

    11 - Symfony : Le param converter

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\Program;
-use App\Entity\Season;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Program;
+use App\Entity\Season;
+use App\Entity\Episode;
 
 /**
  * @Route("/programs", name="program_")
@@ -35,18 +36,8 @@ class ProgramController extends AbstractController
      * @Route("/show/{id<^[0-9]+$>}", name="show")
      * @return Response
      */
-    public function show(int $id):Response
+    public function show(Program $program):Response
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $id]);
-
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'No program with id : '.$id.' found in program\'s table.'
-            );
-        }
-
         $seasons = $program->getSeasons();
 
         return $this->render('program/show.html.twig', [
@@ -56,19 +47,11 @@ class ProgramController extends AbstractController
     }
 
     /**
-     * @Route("/{programId}/seasons/{seasonId}", name="season_show")
+     * @Route("/{program}/seasons/{season}", name="season_show")
      * @return Response
      */
-    public function showSeason(int $programId, int $seasonId)
+    public function showSeason(Program $program, Season $season)
     {
-        $program = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findOneBy(['id' => $programId]);
-
-        $season = $this->getDoctrine()
-            ->getRepository(Season::class)
-            ->findOneBy(['id' => $seasonId]);
-
         $episodes = $season->getEpisodes();
 
         return $this->render('program/season_show.html.twig', [
@@ -77,5 +60,17 @@ class ProgramController extends AbstractController
             'episodes' => $episodes,
         ]);
 
+    }
+
+    /**
+     * @Route("/{program}/seasons/{season}/episodes/{episode}", name="episode_show")
+     */
+    public function showEpisode(Program $program, Season $season, Episode $episode)
+    {
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode,
+        ]);
     }
 }
