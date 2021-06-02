022bc9f344832e7990ba2036a1e63f2560bf10a4
Quentin-BLANC <quentinblanc587@gmail.com>
2021-06-02 12:39:55 +0200

    12 - Symfony : Gardez la “form” !

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
-use App\Entity\Program;
-use Exception;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Category;
+use App\Entity\Program;
+use Exception;
+use App\Form\CategoryType;
 
 /**
 * @Route("/categories", name="category_")
@@ -24,6 +26,38 @@ class CategoryController extends AbstractController
             ->findAll();
 
         return $this->render('category/index.html.twig', ['categories' => $categories]);
+    }
+
+    /**
+     * The controller for the category add form
+     * Display the form or deal with it
+     * 
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        //Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+        // Render the form
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
     }
 
     /**
