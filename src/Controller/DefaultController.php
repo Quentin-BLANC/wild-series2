2c5c702975a45a2b64d49f4b6a831d30cd4f610b
Quentin-BLANC <quentinblanc587@gmail.com>
2021-09-26 21:10:50 +0200

    22 - Symfony : Inscription d’un utilisateur

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,14 +4,24 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ProgramRepository;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
      */
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('index.html.twig');
+        $programs = $programRepository->findBy([], ['id' => 'DESC'], 5);
+        return $this->render('index.html.twig', ['programs' => $programs]);
+    }
+
+    /**
+     * @Route("/my-profile", name="app_profil")
+     */
+    public function profil(): Response
+    {
+        return $this->render('profil.html.twig');
     }
 }
