5f82e98977997e08898ee4ab4f816f6b24ed79f2
Quentin-BLANC <quentinblanc587@gmail.com>
2021-09-27 17:06:20 +0200

    23. - Symfony : Requêtes personnalisées

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -12,6 +12,8 @@ use App\Entity\Episode;
 use App\Entity\Comment;
 use App\Form\ProgramType;
 use App\Form\CommentType;
+use App\Form\SearchProgramFormType;
+use App\Repository\ProgramRepository;
 use App\Service\Slugify;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Mailer\MailerInterface;
@@ -29,14 +31,22 @@ class ProgramController extends AbstractController
      * @Route("/", name="index")
      * @return Response A responce instance
      */
-    public function index(): Response
+    public function index(Request $request, ProgramRepository $programRepository): Response
     {
-        $programs = $this->getDoctrine()
-            ->getRepository(Program::class)
-            ->findAll();
+        $form = $this->createForm(SearchProgramFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['search'];
+            $programs = $programRepository->findLikeName($search);
+        } else {
+            $programs = $programRepository->findAll();
+        }
 
         return $this->render('program/index.html.twig', [
-            'programs' => $programs]);
+            'programs' => $programs,
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
