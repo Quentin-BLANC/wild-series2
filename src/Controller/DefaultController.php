2d5d875f947fda6bcf01ce018618727ff49f9f49
Quentin-BLANC <quentinblanc587@gmail.com>
2021-09-29 22:35:49 +0200

    27 - Symfony : Embedding Controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controller;
+
+use App\Repository\CategoryRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,5 +25,12 @@ class DefaultController extends AbstractController
     public function profil(): Response
     {
         return $this->render('profil.html.twig');
+    }
+
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('layout/navbartop.html.twig', [
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
+        ]);
     }
 }
